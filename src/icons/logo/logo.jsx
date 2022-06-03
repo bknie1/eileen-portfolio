2a4e8738164f052a9d52ddbc86e983275e7fac42
Brandon Knieriem <bmknieriem@ucdavis.edu>
2022-06-03 12:30:08 -0700

    Updates logo dimensions

--- src/icons/logo/logo.jsx
+++ src/icons/logo/logo.jsx
@@ -2,8 +2,7 @@ import * as React from "react"
 
 const Logo = (props) => (
   <svg
-    width={16}
-    height={16}
+    viewBox="0 0 16 16"
     id="SVGRoot"
     xmlns="http://www.w3.org/2000/svg"
     {...props}
