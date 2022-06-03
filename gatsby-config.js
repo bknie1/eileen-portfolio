5ba5f4a809f2e2f63a04081a3b43e67801d692e0
Brandon Knieriem <bmknieriem@ucdavis.edu>
2022-06-03 12:32:56 -0700

    Updates site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,14 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-jodie/gatsby-config.js
-    siteTitle: `Jodie`,
-    siteTitleAlt: `Jodie - Gatsby Starter Portfolio`,
-    siteHeadline: `Jodie - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://jodie.lekoarts.de`,
-    siteDescription: `Image-heavy photography portfolio with colorful accents & customizable pages. Includes adaptive image grids powered by CSS grid and automatic image integration into projects.`,
+    siteTitle: `Eileen Hoang`,
+    siteTitleAlt: `Eileen Hoang - Actor`,
+    siteHeadline: `Eileen - Actor && Artist`,
+    siteUrl: `https://www.eileenhoang.com`,
+    siteDescription: `A portfolio site for actor Eileen Hoang`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `@bknie1`,
   },
   plugins: [
     {
